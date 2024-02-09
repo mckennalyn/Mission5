efdfd70c05cd283a110415a822df579b7c2d4b05
mckennalyn <131218663+mckennalyn@users.noreply.github.com>
2024-02-09 00:32:35 -0700

    Add project files.

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission5.Controllers
+{
+    [Route("calculator")]
+    public class CalculatorController : Controller
+    {
+        [Route("index")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ wwwroot/js/calculator.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+    $('#calculateBtn').click(function () {
+        var hours = $('#hours').val();
+
+        // Validate if hours is a positive number
+        if (isNaN(hours) || parseFloat(hours) <= 0) {
+            if (parseFloat(hours) < 0) {
+                alert('Please enter a positive number for hours.');
+            } else {
+                alert('Please enter a valid number for hours.');
+            }
+            return;
+        }
+
+        var chargePerHour = parseFloat($('#chargePerHour').val().replace('$', ''));
+        var total = hours * chargePerHour;
+        $('#total').val('$' + total.toFixed(2));
+    });
+});
